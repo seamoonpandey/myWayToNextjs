0bf5035f8b6892336e490acc89c16675ca84285c
Seamoon Pandey <hackedasocial@gmail.com>
2022-12-19 10:06:59 +0545

    added styling and some more components

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,7 +2,10 @@ import Link from "next/link";
 import classes from './button.module.css'
 
 function Button(props) {
-    return <Link href={props.link} className={classes.btn}>{props.children}</Link>
+    if (props.link) {
+        return <Link href={props.link} className={classes.btn}>{props.children}</Link>
+    }
+    return <button onClick={props.onClick} className={classes.btn}>{props.children}</button>
 }
 
 export default Button;
